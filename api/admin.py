c27f6680cf7120a80a1fedd2cfb6f909de1580ca
rajababu027 <rajababu12271998@gmail.com>
2023-01-14 11:58:12 +0530

    change

--- api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import VideosDetails, Location, Item, UserDetails
-# Register your models here.
-
-admin.site.register(Location)
-admin.site.register(Item)
-admin.site.register(VideosDetails)
-admin.site.register(UserDetails)
--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storage.settings')
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testingdeployrailwayapp.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
