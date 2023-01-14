ab279e2f8b8b6259360b0493b0a628f1a6d51532
rajababu027 <rajababu12271998@gmail.com>
2023-01-14 12:14:24 +0530

    changee

--- testingdeployrailwayapp/settings.py
+++ testingdeployrailwayapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!$082-1#e$o2w^b%p410fkxv8spnbrd__1$#(uh=gkq)%m==8w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'testingdeployrailwayapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
